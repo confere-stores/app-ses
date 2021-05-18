31b87c086751aab8e666a5f4f46f6d52ff1110d0
João Escudero <joaovescudero@gmail.com>
2021-05-18 17:20:33 -0300

    feat: last update on ses

--- bin/web.js
+++ bin/web.js
@@ -12,8 +12,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const router = express.Router()
+const morgan = require('morgan')
 const port = process.env.PORT || 3000
 
+app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
